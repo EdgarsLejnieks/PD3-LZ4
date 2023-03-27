4b60360b08cf78769a2bc1f6644ba3914da8e660
Edgars Lejnieks <elejnieks867@gmail.com>
2023-03-27 18:11:33 +0300

    Add main and gitignore

---
+++ main.java
@@ -0,0 +1,41 @@
+// LZ4
+
+// Code Conventions:
+// classes -    Donkey, DonkeyKong
+// interfaces - Donkey, DonkeyKong
+// methods -    donkey, donkeyKong
+// variables -  donkey, donkeyKong
+// constants -  DONKEY, DONKEYKONG
+
+class Algorithm {
+    public static void comp(String sourceFile, String resultFile){
+        //TODO: Implement the encoding component of the algorithm
+    }
+
+    public static void decomp(String sourceFile, String resultFile){
+        //TODO: Implement the decoding componenet of the algorithm
+    }
+
+    public static void size(String sourceFile){
+        //TODO: Implement function that checks file size in bytes
+    }
+
+    public static void equal(String sourceFile, String sourceFile2){
+        //TODO: Implement function that checks if two files are the same
+    }
+
+}
+
+class Main {
+    public static void main(){
+
+    }
+    public static void about(){
+        System.out.println("221RDB168, Edgars Lejnieks, 7. grupa");
+        System.out.println("221RDB151, Ričards Ivars Uldukis, 7. grupa ");
+        System.out.println("221RDB195, Normunds Paserns, 7.grupa");
+        System.out.println("221RDB162, Roberts Andris Barlots, 7.grupa");
+        System.out.println("220RX0898, Elvis Avotiņš, 7. grupa");
+
+    }
+}
