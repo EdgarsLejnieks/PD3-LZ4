2e76327c1c9697b72815459fb0bdddd59dfade57
ElvisAvotins <124890311+ElvisAvotins@users.noreply.github.com>
2023-04-12 11:54:01 +0300

    Added equal, size, Main - menu navigation

--- Main.java
+++ Main.java
@@ -7,35 +7,108 @@
 // variables -  donkey, donkeyKong
 // constants -  DONKEY, DONKEYKONG
 
-class Algorithm {
-    public static void comp(String sourceFile, String resultFile){
-        //TODO: Implement the encoding component of the algorithm
-    }
-
-    public static void decomp(String sourceFile, String resultFile){
-        //TODO: Implement the decoding componenet of the algorithm
-    }
-
-    public static void size(String sourceFile){
-        //TODO: Implement function that checks file size in bytes
-    }
-
-    public static void equal(String sourceFile, String sourceFile2){
-        //TODO: Implement function that checks if two files are the same
-    }
-
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String choiseStr;
+		String sourceFile, resultFile, firstFile, secondFile;
+		
+		loop: while (true) {
+			
+			choiseStr = sc.next();
+								
+			switch (choiseStr) {
+			case "comp":
+				System.out.print("source file name: ");
+				sourceFile = sc.next();
+				System.out.print("archive name: ");
+				resultFile = sc.next();
+				comp(sourceFile, resultFile);
+				break;
+			case "decomp":
+				System.out.print("archive name: ");
+				sourceFile = sc.next();
+				System.out.print("file name: ");
+				resultFile = sc.next();
+				decomp(sourceFile, resultFile);
+				break;
+			case "size":
+				System.out.print("file name: ");
+				sourceFile = sc.next();
+				size(sourceFile);
+				break;
+			case "equal":
+				System.out.print("first file name: ");
+				firstFile = sc.next();
+				System.out.print("second file name: ");
+				secondFile = sc.next();
+				System.out.println(equal(firstFile, secondFile));
+				break;
+			case "about":
+				about();
+				break;
+			case "exit":
+				break loop;
+			}
+		}
+		sc.close();
 }
 
-class Main {
-    public static void main(){
+public static void comp(String sourceFile, String resultFile){
+    //TODO: Implement the encoding component of the algorithm
+}
+public static void decomp(String sourceFile, String resultFile){
+    //TODO: Implement the decoding componenet of the algorithm
+}
 
-    }
-    public static void about(){
+public static void size(String sourceFile) {
+	try {
+		FileInputStream f = new FileInputStream(sourceFile);
+		System.out.println("size: " + f.available());
+		f.close();
+	}
+	catch (IOException ex) {
+		System.out.println(ex.getMessage());
+	}
+}
+
+public static boolean equal(String firstFile, String secondFile) {
+	try {
+		FileInputStream f1 = new FileInputStream(firstFile);
+		FileInputStream f2 = new FileInputStream(secondFile);
+		int k1, k2;
+		byte[] buf1 = new byte[1000];
+		byte[] buf2 = new byte[1000];
+		do {
+			k1 = f1.read(buf1);
+			k2 = f2.read(buf2);
+			if (k1 != k2) {
+				f1.close();
+				f2.close();
+				return false;
+			}
+			for (int i=0; i<k1; i++) {
+				if (buf1[i] != buf2[i]) {
+					f1.close();
+					f2.close();
+					return false;
+				}				
+            }
+			} while (k1 == 0 && k2 == 0);
+			    f1.close();
+			    f2.close();
+			    return true;
+		    }
+		catch (IOException ex) {
+			System.out.println(ex.getMessage());
+			return false;
+		}
+}
+
+public static void about(){
         System.out.println("221RDB168, Edgars Lejnieks, 7. grupa");
         System.out.println("221RDB151, Ričards Ivars Uldukis, 7. grupa ");
         System.out.println("221RDB195, Normunds Paserns, 7.grupa");
         System.out.println("221RDB162, Roberts Andris Barlots, 7.grupa");
         System.out.println("220RX0898, Elvis Avotiņš, 7. grupa");
-
-    }
 }
